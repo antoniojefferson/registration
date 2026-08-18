c2d4735ca537e2613818fbf70ea164f101480fba
Antonio Jefferson <antoniojeferson96@gmail.com>
2026-08-17 21:02:26 -0300

    feat: atualizando o projeto para o rails 8

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'database_cleaner'
-require 'capybara/rspec'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
